e779f46957e55c70cb3ba182ae34acbf3cb027c7
LucasWagner97 <147665533+LucasWagner97@users.noreply.github.com>
2023-10-27 00:36:55 +0000

    atualização 2

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-const gato = {
-    nome: "Tom",
-    idade: 4,
-    raça: "viralata",
-    miar () {
-        console.log(this.nome)
+class C6Bank{
+    nome: string
+    id: number
+    senha: number
+    saldo = 0
+    constructor(nomeFornecido: string, idFornecido: number, senhaFornecida: number) {
+        this.nome = nomeFornecido
+        this.id = idFornecido
+        this.senha = senhaFornecida
     }
-
 }
 
-gato.miar()
+const contaLucas = new C6Bank("Lucas", 928172198, 672562)
+
+console.log(contaLucas.nome)
